446a039667fb32a106c306f679d480e921822feb
Athena Rubio <75556016+SpeedyDolphin@users.noreply.github.com>
2025-07-07 18:28:09 -0700

    added funtionality to the add row buttons

--- scripts/config/pathConfigSettings.js
+++ scripts/config/pathConfigSettings.js
@@ -1,3 +1,46 @@
+const DEFAULT_PATHS = {
+  "a5e": {
+    "default": {
+      "path": "system.attributes.movement.walk.distance",
+      "label": "walk"
+    },
+    "movementSpeeds": [
+      {
+        "type" : "dictionary",
+        "path" : "system.attributes.movement",
+        "value": ".distance"
+      }
+    ]
+  },
+  "dnd5e" : {
+    "default": {
+      "path": "system.attributes.movement.walk",
+      "label": "walk"
+    },
+    "movementSpeeds": [
+      {
+        "type" : "dictionary",
+        "path" : "system.attributes.movement",
+        "value": ""
+      }
+    ]
+  },
+  "pf2e" : {
+    "default": {
+      "path": "system.attributes.speed.total",
+      "label": "land speed"
+    },
+    "movementSpeeds": [
+      {
+        "type" : "array",
+        "path" : "system.attributes.movement.otherSpeeds",
+        "value": ".value",
+        "label": ".label"
+      }
+    ]
+  }
+}
+
 export class PathConfigSettings extends FormApplication {
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
@@ -16,33 +59,52 @@ export class PathConfigSettings extends FormApplication {
   }
 
   activateListeners(html) {
-  super.activateListeners(html);
+    super.activateListeners(html);
 
-  // Handle the click event for the collapsible rows
-  html.find(".athena-collapsible-row").on("click", (event) => {
-    event.preventDefault();
-    const $header = $(event.currentTarget);
-    const $content = $header.next(".athena-collapsible-row-content");
-    $content.toggle();
-    const $caret = $header.find(".caret i");
-    $caret.toggleClass("athena-rotated");
-  });
-  //Handle the click event so clicking on the input doesn't toggle the dropdown
-  html.find(".path-field").on("click", (event) => {
-    event.stopPropagation();
-  });
-  // Handle the click event for deleting rows
-  html.find(".athena-delete-row").on("click", (event) => {
-    event.stopPropagation();
-    $(event.currentTarget).closest(".athena-entry").remove();
-  });
-  // Handle the click event for adding a new entry
-  html.find(".athena-add-entry").on("click", (event) => {
-    event.stopPropagation();
-    $(".athena-dropdown-add-menu").toggle();
-  });
-}
+    // Handle the click event for the collapsible rows
+    html.on("click", ".athena-collapsible-row", (event) => {
+      event.preventDefault();
+      const $header = $(event.currentTarget);
+      const $content = $header.next(".athena-collapsible-row-content");
+      $content.toggle();
+      const $caret = $header.find(".caret i");
+      $caret.toggleClass("athena-rotated");
+    });
+    //Handle the click event so clicking on the input doesn't toggle the dropdown
+    html.on("click", ".path-field", (event) => {
+      event.stopPropagation();
+    });
+    // Handle the click event for deleting rows
+    html.on("click", ".athena-delete-row", (event) => {
+      event.stopPropagation();
+      $(event.currentTarget).closest(".athena-entry").remove();
+    });
+    // Handle the click event for adding a new entry
+    html.on("click", ".athena-add-entry", (event) => {
+      event.stopPropagation();
+      $(".athena-dropdown-add-menu").toggle();
+    });
+    html.on("click", ".add-option", async (event) => {
+      event.stopPropagation();
+      $(".athena-dropdown-add-menu").toggle();
 
+      const type = $(event.currentTarget).data("type");
+      let row; 
+      switch (type){
+          case "manual":
+            row = $( await renderTemplate("modules/athenas-movement-trail/templates/partials/manualEntry.hbs", {}))
+            break;
+          case "dictionary":
+            row = $( await renderTemplate("modules/athenas-movement-trail/templates/partials/dictEntry.hbs", {}))
+            break;
+          case "array":
+            row = $( await renderTemplate("modules/athenas-movement-trail/templates/partials/arrayEntry.hbs", {}))
+            break;
+      }
+      $(".athena-table").append(row)
+    });
+
+  }
   async _updateObject(event, formData) {
     console.log("Form submitted:", formData);
   }
